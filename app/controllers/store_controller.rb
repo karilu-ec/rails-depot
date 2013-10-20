f75ec55b274b377f56c69c48e23ecbddec29e8b3
Karina Simpson <karilu_ec@yahoo.com>
2013-10-19 22:30:03 -0400

    Adding jquery to display the cart on the sidebar with ajax

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,12 +1,16 @@
 class StoreController < ApplicationController
   include StoreCounter
+  include CurrentCart
   before_action :counter_set, only: [:index]
+  before_action :set_cart
   
   def index
     @products = Product.order(:title)
     @mytime = Time.now
     @count = session[:counter]  
     @message_counter = "You have accessed the application #{@count} times"  if @count > 5 
+  ## this was part of my solution to not display the cart on the left
+  ##@cartemptyFlag = @cart.find_products(@cart.id)
   end
   
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -11,15 +11,16 @@ class Cart < ActiveRecord::Base
     current_item
   end
   
-  def find_products(cart_id)
-    current_item = line_items.find_by(cart_id: cart_id)
-    if current_item
-      current_item
-    else
-      current_item = nil
-    end
-    current_item
-  end
+  # My solution for not displaying the empty cart.##
+  #def find_products(cart_id)
+  #  current_item = line_items.find_by(cart_id: cart_id)
+  #  if current_item
+  #    current_item
+  #  else
+  #    current_item = nil
+  #  end
+  #  current_item
+  #end
   
   def total_price
     line_items.to_a.sum {|item| item.total_price}
